579646705c162087821000d8ceaa377c0080d1d4
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-21 01:10:28 +0000

    beginning of the playlist app

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log(__dirname);
+console.log(__filename);
+
+// var time = 0; 
+
+
+
+// var timer = setInterval(function(){
+//     time += 2; 
+//     console.log(time + ' 3 second have passed');
+//     if (time > 5) { 
+//         clearInterval(time); 
+//         }
+// }, 2000);
