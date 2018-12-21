5e5fd3bfd55c65d0386d2f3474707bfa3e91aaa2
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-21 02:25:03 +0000

    one more modules that we added

--- stuff.js
+++ stuff.js
@@ -1,13 +1,17 @@
-module.exports.counter = function(arr) { return 'There are ' + arr.length + ' elements in this array';
+var counter = function(arr) { return 'There are ' + arr.length + ' elements in this array';
 };
 
 
-module.exports.adder = function(a,b) { 
+var adder = function(a,b) { 
     return `The sume of the 2 numbers is ${a+b}`;
 
 };
-
-module.exports.pi = 3.142; 
-
+var pi = 3.142; 
 
 
+module.exports = { 
+    counter: counter, 
+    adder: adder, 
+    pi: pi
+    
+};
