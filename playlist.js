49ea94cbc7fec0be594aa97fab75ca34d530838c
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-21 01:21:15 +0000

    playlist expressions

---
+++ playlist.js
@@ -0,0 +1,18 @@
+function callFunction(fun){
+    fun();
+}
+
+// function sayHi() { 
+//     console.log("hi");
+// }
+
+// sayHi();
+
+//function expression 
+var sayBye = function(){
+console.log("bye");
+};
+
+//sayBye();
+
+callFunction(sayBye);
