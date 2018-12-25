fa8d3ea3357a904660724c495cd5e631c63d302a
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-25 01:41:19 +0000

    jSON application in node.js

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ var fs = require('fs');
  var server = http.createServer(function(req, res){
      
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': "text/html"});
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
-    myReadStream.pipe(res);  
-    res.end('Hey ninjas');
+    res.writeHead(200, {'Content-Type': "application/json"});
+    var myObj = { 
+        name: "Ryu", 
+        job: 'Ninja',
+        age: 29
+    };
+    res.end(JSON.stringify(myObj));
+    
+ });
+   
+   
+  // var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
+    // myReadStream.pipe(res);  
+    // res.end('Hey ninjas');
 
     
     
     
- });
+ 
 
 
  server.listen(3000, '127.0.0.1');
