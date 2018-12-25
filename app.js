f7c1abc91ff20b687705bd5f568d149f75971ee8
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-25 01:04:28 +0000

    Creating pipes in node.js

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-var http = ("http");
+var http = require("http");
 var fs = require('fs');
+ 
+ var server = http.createServer(function(req, res){
+     
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': "text/plain"});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
+    myReadStream.pipe(res);  
+    res.end('Hey ninjas');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-myReadStream.on('data' , function(chunk){
-    console.log('new chunk received'); 
-    console.log(chunk);
-    myWriteStream.write(chunk);
-});
-
-
-
-
-
-
-
-
-
-
-
-// var http = require('http'); 
-
-// var server = http.createServer(function(req, res){
-    
-//     res.writeHead(200, {'Content-Type': "text/plain"});
-//     res.end("Hey Ninjas"); 
     
     
     
-// });
+ });
 
 
-// server.listen(3000, '127.0.0.1');
-// console.log("yo dawgs now listening to port 3000");
+ server.listen(3000, '127.0.0.1');
+ console.log("yo dawgs now listening to port 3000");
