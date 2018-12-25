a2d0eb1f8de4339e016cc8e81d5e1030abbdc8d0
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-25 06:02:56 +0000

    routing

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ var fs = require('fs');
  var server = http.createServer(function(req, res){
      
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': "application/json"});
-    var myObj = { 
-        name: "Ryu", 
-        job: 'Ninja',
-        age: 29
-    };
-    res.end(JSON.stringify(myObj));
-    
+    if (req.url === '/home || req.url === '/''){
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        fs.createReadStream(__dirname + 'index.html').pipe(res);
+    } else if(req.url === '/contact'){
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        fs.createReadStream(__dirname + 'contact.html').pipe(res);
+    }else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(ninjas));
+        
+    } else { 
+        res.writeHead(404, {'Content0-Type': '404/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }
  });
+    
    
    
-  // var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
+ // var myReadStream = fs.createReadStream(__dirname + '/readMe.txt' , 'utf8');
     // myReadStream.pipe(res);  
     // res.end('Hey ninjas');
-
-    
-    
-    
- 
-
-
  server.listen(3000, '127.0.0.1');
  console.log("yo dawgs now listening to port 3000");
--- readMe.txt
+++ readMe.txt
@@ -1,1 +1,14 @@
-yay you read me bitch Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+var http = require('http'); 
+
+ var server = http.createServer(function(req, res){
+    
+     res.writeHead(200, {'Content-Type': "text/plain"});
+     res.end("Hey Ninjas"); 
+    
+    
+    
+ });
+
+
+ server.listen(3000, '127.0.0.1');
+ console.log("yo dawgs now listening to port 3000");
