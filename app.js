99b23871be60eca9def0de8960515e7fae69e3a9
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-25 18:28:33 +0000

    express

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get('/contact', function(req,res){
     res.send("this is the contact page");
 });
 
+app.get('/profile/:id', function(req, res){
+    res.send('you requested to see a profile with the id of ' + req.params.id)
+})
+
 app.listen(3000)
